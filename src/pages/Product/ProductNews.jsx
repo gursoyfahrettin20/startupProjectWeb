c3099a8750a155949013f3937edb0feec428a7f4
gursoyfahrettin20 <110720110+gursoyfahrettin20@users.noreply.github.com>
2024-05-12 19:19:46 +0300

    main
    Ürün, Ana sayfa, Api, Menü , Rooter işlemleri

---
+++ src/pages/Product/ProductNews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductNews(props) {
+    return (
+        <div>
+            Burası daha sonra yapılacak
+        </div>
+    );
+}
+
+export default ProductNews;
