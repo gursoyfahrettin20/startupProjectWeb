2c8346ceeda9263db53fa1b66d0388e3fc0aa219
gursoyfahrettin20 <110720110+gursoyfahrettin20@users.noreply.github.com>
2024-05-19 04:24:23 +0300

    master
    link güncelleme

--- src/components/leftMenu/index.jsx
+++ src/components/leftMenu/index.jsx
@@ -29,8 +29,8 @@ const Index = () => {
             getItem(<a href="/mailSettings">Mail Ayarları</a>, 'mailSettings', <MailOutlined/>),
             getItem(<a href="/siteSettings">Site Ayarları</a>, 'siteSettings', <SettingOutlined/>),
         ]),*/
-        getItem('Ana Sayfa İşlemleri', 'mainPage', <AppstoreOutlined/>, [
-            getItem("Ana Sayfa", 'aboutUs', <ReadOutlined/>, [
+        getItem('Ana Sayfa İşlemleri', 'mainPageAndSliderAndNews', <AppstoreOutlined/>, [
+            getItem("Ana Sayfa", 'mainPages', <ReadOutlined/>, [
                 getItem(<a href="/mainPage">Manşet-1</a>, 'mainHeadline1', <SlidersOutlined/>),
                 getItem(<a href="/mainHeadline">Manşet-2</a>, 'mainHeadline2', <SlidersOutlined/>),
                 getItem(<a href="/breakHeadline">Manşet-3</a>, 'mainHeadline3', <SlidersOutlined/>),
@@ -39,12 +39,11 @@ const Index = () => {
             getItem(<a href="/slider">Slider</a>, 'slider', <SlidersOutlined/>),
             getItem(<a href="/news">Haber Slider</a>, 'news', <SlidersOutlined/>)
         ]),
-        getItem('Ürün Kategorisi', 'products', <ShareAltOutlined/>, [
+        getItem('Ürün Kategorisi', 'categoryAndProduct', <ShareAltOutlined/>, [
             getItem(<a href="/categories">Kategori Oluştur / Listele</a>, 'categories', <AppstoreAddOutlined/>),
             getItem(<a href="/product">Ürün Oluştur / Listele</a>, 'product', <MobileOutlined/>,),
-            // getItem(<a href="/productNews">Ürünlerin Sayfa Bazında <br/> Haber Sayfası</a>, 'product', <MobileOutlined/>)
         ]),
-        getItem('Site Detay', 'about', <InfoCircleOutlined/>, [
+        getItem('Site Detay', 'siteDetail', <InfoCircleOutlined/>, [
             getItem(<a href="/aboutUs">Hakkımızda</a>, 'aboutUs', <ReadOutlined/>),
             getItem(<a href="/ourVision">Vizyonumuz</a>, 'ourVision', <FlagOutlined/>),
             getItem(<a href="/ourMission">Misyonumuz</a>, 'ourMission', <FlagOutlined/>),
